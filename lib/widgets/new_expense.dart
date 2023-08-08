409edd5e006e2d4a5ca67c305b291970acc6c964
Soosan Shabnam <soosan2609@gmail.com>
2023-08-08 15:37:50 +0530

    changes

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,21 +8,51 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController= TextEditingController();
+  final _amountController= TextEditingController();
+  void dispose()
+  {
+    _titleController.dispose();
+    _amountController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:const EdgeInsets.all(16),
-      child:Column(
-        children: const [
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
           TextField(
-            maxLength:50,
-            decoration: InputDecoration(
+            controller: _titleController,
+            maxLength: 50,
+            decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+          TextField(
+            controller: _amountController,
+            keyboardType: TextInputType.number,
+            decoration: const InputDecoration(
+              prefixText: '\$',
+              label: Text('Amount'),
+            ),
+          ),
+          Row(children: [
+            TextButton(onPressed:(){
+              Navigator.pop(context);
+            }, child: Text('Cancel'),),
+            ElevatedButton(
+              onPressed: () {
+                print(_titleController.text);
+                print(_amountController.text);
+              },
+              child: const Text('Save Expenses '),
+            ),
+          ])
         ],
       ),
-
     );
   }
 }
