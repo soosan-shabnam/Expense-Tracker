efde5384843dc3a1b7a53fa88077013d90267edd
Soosan Shabnam <soosan2609@gmail.com>
2023-08-10 13:43:47 +0530

    final

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -42,6 +42,9 @@ class ExpenseBucket{
     required this.category,
     required this.expenses,
 });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category): expenses=allExpenses.where((expense) => expense.category==category).toList();
+
   final Category category;
   final List<Expense> expenses;
 
